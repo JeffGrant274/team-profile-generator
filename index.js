290a68d2404601574eccc9103c287e130a6ac05a
Jeff Grant <jgrant274@gmail.com>
2021-07-13 21:43:11 -0400

    working on prompts

--- index.js
+++ index.js
@@ -1,46 +1,160 @@
-const manager = require ('./lib/manager')
-const Engineer = require ('./lib/engineer')
-const intern = require ('./lib/intern')
-const fs = require('fs');
-const inquirer = require ('inquirer')
-const genFilePath = "./dist/profile.html"
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const genFilePath = "./dist/profile.html";
 
+let staff = [];
 
 // TODO: Create an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    message: "Please Enter a managers name?",
-    name: "managerName",
-  },
-  {
-    type: "input",
-    message: "Please enter their employee ID",
-    name: "managerId",
-  },
-  {
-    type: "input",
-    message: "Please enter their email address",
-    name: "managerEmail",
-  },
-  {
-    type: "input",
-    message: "Please enteir their office number",
-    name: "managerOffNum",
-  },
-  {
-      type:"checkbox",
-      message:"Would you like to add a team member?  Select the role below.",
-      choices:["Engineer", "Intern", "Exit"],
-      name: "addStaff"
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please Enter a managers name?",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "Please enter their employee ID",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "Please enter their email address",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "Please enteir their office number",
+      name: "managerOffNum",
+    },
+    {
+      type: "checkbox",
+      message: "Would you like to add a team member?  Select the role below.",
+      choices: ["Engineer", "Intern", "Exit"],
+      name: "moreStaff",
+    },
+  ])
+    .then((answers) => {
+        let manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOffeNum
+        );
+        staff.push(manager);
+        if (answers.moreStaff === "Intern") {
+          addIntern();
+        } else if (answers.moreStaff === "Engineer") {
+          addEngineer();
+        } else {
+          EndofPrompts();
+        }
+      });
 
-  }
-])
-.then(answers => {
-    fs.writeFileSync(genFilePath,"")
-})
+  function addIntern()
+{
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please Enter an intern's name?",
+      name: "internName",
+    },
+    {
+      type: "input",
+      message: "Please enter their employee ID",
+      name: "internId",
+    },
+    {
+      type: "input",
+      message: "Please enter their email address",
+      name: "internEmail",
+    },
+    {
+      type: "input",
+      message: "Please enteir their school",
+      name: "internSchool",
+    },
+    {
+      type: "checkbox",
+      message: "Would you like to add a team member?  Select the role below.",
+      choices: ["Engineer", "Intern", "Exit"],
+      name: "moreStaff",
+    },
+  ])
+.then((answers) => {
+    let intern = new Intern(
+      answers.internName,
+      answers.internId,
+      answers.internEmail,
+      answers.internSchool
+    );
+    staff.push(intern);
+    if (answers.moreStaff === "Intern") {
+      addIntern();
+    } else if (answers.moreStaff === "Engineer") {
+      addEngineer();
+    } else {
+      EndofPrompts();
+    }
+    // fs.writeFileSync(genFilePath,"")
+  });
+}
 
+function addEngineer()
+{
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please Enter an engineer's name?",
+      name: "engineerName",
+    },
+    {
+      type: "input",
+      message: "Please enter their employee ID",
+      name: "engineerId",
+    },
+    {
+      type: "input",
+      message: "Please enter their email address",
+      name: "engineerEmail",
+    },
+    {
+      type: "input",
+      message: "Please enter their Github username",
+      name: "enginnerGitHub",
+    },
+    {
+      type: "checkbox",
+      message: "Would you like to add a team member?  Select the role below.",
+      choices: ["Engineer", "Intern", "Exit"],
+      name: "moreStaff",
+    },
+  ])
+  .then((answers) => {
+    let engineer = new Engineer(
+      answers.engineerName,
+      answers.engineerId,
+      answers.engineerEmail,
+      answers.internSchool
+    );
+    staff.push(engineer);
+    if (answers.moreStaff === "intern") {
+      addIntern();
+    } else if (answers.moreStaff === "engineer") {
+      addEngineer();
+    } else {
+      EndofPrompts();
+    }
+    // fs.writeFileSync(genFilePath,"")
+  });
+}
 
+// function EndofPrompts
 
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
