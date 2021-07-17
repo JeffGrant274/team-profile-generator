7df122bb0d52b594fd2229f23381d025f9f49b6a
Jeff Grant <jgrant274@gmail.com>
2021-07-17 13:06:55 -0400

    finally got unique line generated.  Final push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//classes we brought over to create our generator
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+//packages and file paths we need to generate our CLI
 const fs = require("fs");
 const inquirer = require("inquirer");
 const genFilePath = "./dist/profile.html";
@@ -8,14 +10,16 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 const log = console.log;
 
+//empty array to store user generated content
 let staff = [];
 
+//figlet line to generate our CLI title, coloured blue with chalk
 log(
   chalk.blue(
     figlet.textSync("Team Profile Generator", { horizontalLayout: "full" })
   )
 );
-// TODO: Create an array of questions for user input
+//prompts generating manager information
 inquirer
   .prompt([
     {
@@ -45,6 +49,7 @@ inquirer
       name: "moreStaff",
     },
   ])
+  //generating the new manager
   .then((answers) => {
     let manager = new Manager(
       answers.managerName,
@@ -52,16 +57,21 @@ inquirer
       answers.managerEmail,
       answers.managerOffNum
     );
+  //pushing the manager to staff array
     staff.push(manager);
+  //validating if user wants to add more staff then running the subsequent function for the staff class they user chose
     if (answers.moreStaff === "Intern") {
       addIntern();
     } else if (answers.moreStaff === "Engineer") {
       addEngineer();
     } else {
+    //if user is done generating staff cards run function to end the prompts and generate the markup
+      log("Roster complete, please Navigate to the profile page")
       EndofPrompts();
     }
   });
 
+//intern prompts to generate an intern staff card
 function addIntern() {
   inquirer
     .prompt([
@@ -92,6 +102,7 @@ function addIntern() {
         name: "moreStaff",
       },
     ])
+    //generating new intern
     .then((answers) => {
       let intern = new Intern(
         answers.internName,
@@ -99,17 +110,22 @@ function addIntern() {
         answers.internEmail,
         answers.internSchool
       );
+    //pushing intern to staff array
       staff.push(intern);
+    //validating if user needs to add more staff cards and then running the appropriate function
       if (answers.moreStaff === "Intern") {
         addIntern();
       } else if (answers.moreStaff === "Engineer") {
         addEngineer();
       } else {
+      //if user is done adding staff stopping the prompts and generating page markup
+        log("Roster complete, please Navigate to the profile page")
         EndofPrompts();
       }
     });
 }
 
+//function to generate a new engineer card based of user prompts
 function addEngineer() {
   inquirer
     .prompt([
@@ -141,23 +157,29 @@ function addEngineer() {
       },
     ])
     .then((answers) => {
+      //generating new engineer
       let engineer = new Engineer(
         answers.engineerName,
         answers.engineerId,
         answers.engineerEmail,
         answers.engineerGitHub
       );
+      //pushing engineer to staff array
       staff.push(engineer);
+      // validating if user needs more staff and then running the appropriate function
       if (answers.moreStaff === "Intern") {
         addIntern();
       } else if (answers.moreStaff === "Engineer") {
         addEngineer();
       } else {
+      //when user is done generating staff stops prompts and generates page markup
+        log("Roster complete, please Navigate to the profile page")
         EndofPrompts();
       }
     });
 }
 
+//generates the basic markup for the profile page
 function genStartHtml() {
   return `<!DOCTYPE html>
   <html lang="en">
@@ -178,32 +200,36 @@ function genStartHtml() {
       <div class = "page-box">`;
 }
 
-function specInfo(staff) {
-  if (staff.getRole() === "Manager") {
-    return ` <li class = "list-group-item">Office Number: ${staff.getOffNum()}</li>`;
-  } else if (staff.getRole() === "Intern") {
-    return `<li class = "list-group-item">School: ${staff.getSchool()} </li>`;
+//this function generates the unique line in the card specific to each class
+ function specInfo(staff) {
+   if (staff.getRole() === "Manager") {
+     return ` <li class = "list-group-item">Office Number: ${staff.getOffNum()}</li>`;
+   } else if (staff.getRole() === "Intern") {
+     return `<li class = "list-group-item">School: ${staff.getSchool()} </li>`;
   } else {
-    return ` <li class = "list-group-item">GitHub: ${staff.getGithub()}</li>`;
-  }
-}
+     return ` <li class = "list-group-item">GitHub: <a href = "www.github.com/${staff.getGithub()}">${staff.getGithub()}</a></li>`;
+   }
+ }
 
-function staffHtml(staff) {
+//generates the staff card markup
+function staffHtml(staff) 
+{
   return `
-<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${staff.getName()}</h5>
-          <p class="card-text">${staff.getRole()}</p>
+  <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">${staff.getName()}</h5>
+            <p class="card-text">${staff.getRole()}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Employee Id:${staff.getId()}</li>
+            <li class="list-group-item">Email Address: <a href = "mailto:${staff.getEmail()}">${staff.getEmail()}</a></li>
+            ${specInfo(staff)}
+          </ul>
         </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Employee Id:${staff.getId()}</li>
-          <li class="list-group-item">Email Address: <a href = "mailto:${staff.getEmail()}">${staff.getEmail()}</li>
-          ${specInfo()}
-        </ul>
-      </div>
-`;
+  `
 }
 
+//generates the last of the markup to build the page
 function genFinalHtml() {
   ` </div>  
 
@@ -211,6 +237,7 @@ function genFinalHtml() {
   </html`;
 }
 
+//this function writes and generates the finished product
 function EndofPrompts() {
   fs.writeFileSync(genFilePath, "");
   let markupData = genStartHtml();
@@ -220,4 +247,5 @@ function EndofPrompts() {
   }
   markupData += genFinalHtml();
   fs.writeFileSync(genFilePath, markupData);
-}
+
+  }
